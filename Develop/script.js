3a92ae110d252dd4caa4a90e42e21b06db39234d
John Nielsen <johnhayesnielsen@gmail.com>
2020-05-16 23:31:09 -0600

    fixed functions to make prompts run

--- Develop/script.js
+++ Develop/script.js
@@ -15,27 +15,23 @@ alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  enter = parseInt(prompt("How long would you like your password to be? Please enter a number between 8 and 128"));
-  if (!enter) {
-    alert("Please enter a number between 8 and 128!");
-  } else if (enter < 8 || enter > 128) {
-    enter = parseInt(prompt("Please choose a number between 8 and 128!"));
-
-    } else {
-      confirmLowercase = confirm("Woould you like to use Lowercase Letters in your password?");
-      confirmUppercase - confirm("Would you like to use Uppercase Letters in your password?");
-      confirmNumber = confirm("Would you like to use Numbers in your password?");
-      confirmCharacter = confirm("Would you like to use Special Characters in your password?");
-    } 
-};  
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+// Write password to the #password input
+  password = generatePassword();
+  document.querySelector("#password");
+});  
+
+  function generatePassword() {
+    enter = parseInt(prompt("How long would you like your password to be? Please enter a number between 8 and 128"));
+    if (!enter || enter < 8 || enter > 128) {
+      alert("Please enter a number between 8 and 128!");
+      generatePassword();
+     } else if (enter >= 8 && enter <= 128) {
+        alert('Your password will be ' + enter + ' characters long.');
+        confirmLowercase = confirm("Would you like to use Lowercase Letters in your password?");
+        confirmUppercase - confirm("Would you like to use Uppercase Letters in your password?");
+        confirmNumber = confirm("Would you like to use Numbers in your password?");
+        confirmCharacter = confirm("Would you like to use Special Characters in your password?");
+  } 
+};
